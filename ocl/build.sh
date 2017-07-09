63b39d8f6d8965287fe1e760e6a445732b15a10a
looooo <sppedflyer@gmail.com>
2017-07-10 00:18:40 +0200

    ocl: patch for py3

--- ocl/build.sh
+++ ocl/build.sh
@@ -5,6 +5,7 @@ cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DBOOST_LIBRARYDIR=$PREFIX/lib \
+      -DPYTHON_VERSION_STRING=$PY_VER \
       ../src
 
 make -j4 2>&1 | tee output.txt
